2812fe91c2b4258317fb406d3d485deac7387ff2
sublimation42 <catchtwotwoallstar@gmail.com>
2017-01-13 09:49:10 -0500

    Tests to select accurate rated based on restirctions utilizing one hour worked completed and passed.

--- src/payEstimator.java
+++ src/payEstimator.java
@@ -6,12 +6,22 @@ public class payEstimator {
     {
         Double rate = 0.00;
 
-
+        /* determine rate */
         if (startTime >= 5 && startTime <8)
         {
             rate = 12.00;
         }
+        else if (startTime >= 8 && startTime <12)
+        {
+            rate = 8.00;
+        }
+        else if (startTime >= 12 || startTime < 4)
+        {
+            rate = 16.00;
+        }
 
         return rate;
     }
+
+    /* calualte hours */
 }
--- test/payEstimatorTest.java
+++ test/payEstimatorTest.java
@@ -13,7 +13,22 @@ public class payEstimatorTest {
 
         payEstimator totalFee = new payEstimator();
 
+        /* Testing for accurate rates based on restrictions */
+
+        //testing one hour at 12.00 rate between hours of 5 pm and 8 pm.
         assertEquals(12.00, totalFee.estimatePay(5,6), .001);
+
+        //testing one hour at 8.00 rate between 8 pm and 12 am.
+        assertEquals(8.00, totalFee.estimatePay(8,9), .001);
+
+        //testing one hour at 16.00 rate between 12 am and 4 am.
+        assertEquals(16.00, totalFee.estimatePay(12,1), .001);
+
+        /* Testing accurate rate and hours worked */
+
+        
+
+
 
 
 
