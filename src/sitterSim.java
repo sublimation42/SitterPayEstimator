33c8b64a19ea9d29031b389b6dd926d53d42f39e
sublimation42 <catchtwotwoallstar@gmail.com>
2017-01-16 06:56:18 -0500

    Added valid time test to sitterSim including a return message that comfimrs successful appointment and returns payEstimate for the appointment, tests passed.

--- src/sitterSim.java
+++ src/sitterSim.java
@@ -5,12 +5,15 @@ public class sitterSim {
 
     public String scheduleAppt(Integer startTime, String stPeriod, Integer bedTime, Integer endTime, String etPeriod) {
 
+        payEstimator totalFee = new payEstimator();
+
         if ((startTime < 5 && stPeriod.equalsIgnoreCase("pm")) ||
                 ( endTime > 4 && etPeriod.equalsIgnoreCase("am")))
         {
             return "Sorry, sitting hours are limited to between 5:00 PM to 4:00 AM.";
         }
-        return "hi";
+        return "Sitting appointment complete, your pay is: $" +
+                totalFee.estimatePay(startTime, bedTime, endTime);
 
 
 
--- test/sitterSimTest.java
+++ test/sitterSimTest.java
@@ -11,7 +11,15 @@ public class sitterSimTest {
 
         sitterSim aSitter = new sitterSim();
 
+        //invalid time window
+
        assertEquals("Sorry, sitting hours are limited to between 5:00 PM to 4:00 AM.", aSitter.scheduleAppt(4, "pm",  10, 2, "am"));
+
+       //valid time window
+
+        assertEquals("Sitting appointment complete, your pay is: $64.00.", aSitter.scheduleAppt(6, "pm",  10, 12, "am"));
+
+
 
 
 
